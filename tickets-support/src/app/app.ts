8e389e6e38df09aca153ea2cfd5546a76f6db858
siwar <siwarmliki123@gmail.com>
2026-04-28 10:00:52 +0100

    Add Interface

--- tickets-support/src/app/app.ts
+++ tickets-support/src/app/app.ts
@@ -1,12 +1,37 @@
+
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('tickets-support');
+  form: FormGroup;
+  submitted = false;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      prenom: [''],
+      nom: [''],
+      telephone: [''],
+      email: [''],
+      titre: [''],
+      categorie: ['Question generale'],
+      description: ['']
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.form.valid) {
+      // Here you can send the form data to your backend or service
+      alert('Demande envoyée!\n' + JSON.stringify(this.form.value, null, 2));
+      this.form.reset({ categorie: 'Question generale' });
+      this.submitted = false;
+    }
+  }
 }
